c27a590413d1d4a0ee55e232de44d0f7c6cd0903
vimiPonyai <weimingliu@pony.ai>
2019-01-10 22:01:35 +0800

    cros for all

--- api/user.py
+++ api/user.py
@@ -1,16 +1,17 @@
 from server import app
 
-from flask import session, g, jsonify, make_response
-from decorator.decorator_tools import parse_user, pares_access
+from flask import session, g, jsonify, make_response, request
+from decorator.decorator_tools import parse_user, parse_access
 
 @app.route('/register')
-@pares_access()
+@parse_user()
+@parse_access()
 def register():
     resp = make_response('hello world')
     return resp
 
 @app.route('/')
-@pares_access()
+@parse_access()
 def index():
     session['user_id'] = 2
     return jsonify({"success":1})
--- decorator/decorator_tools.py
+++ decorator/decorator_tools.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from flask import g, session, make_response
+from flask import g, session, make_response, request
 import functools
 
 """
@@ -23,15 +23,15 @@ def parse_user():
 """
     set Access-Control-Allow
 """
-def pares_access(allow_origin = "*", allow_methods = "PUT,GET,POST,DELETE,OPTIONS", allow_headers = "Referer,Accept,Origin,User-Agent"):
+def parse_access(allow_origin = "*", allow_methods = "PUT,GET,POST,DELETE,OPTIONS", allow_headers = "Referer,Accept,Origin,User-Agent"):
     def decorator(func):
         @functools.wraps(func)
         def wrapper_func(*args, **kwargs):
             resp = make_response(func(*args, **kwargs))
-            resp.headers['Access-Control-Allow-Origin'] = allow_origin
+            resp.headers['Access-Control-Allow-Origin'] = request.headers.get('Origin')
             resp.headers['Access-Control-Allow-Methods'] = allow_methods
             resp.headers['Access-Control-Allow-Headers'] = allow_headers
-            resp.headers['Access-Control-Allow-Credentials'] = True # allow send cookies
+            resp.headers['Access-Control-Allow-Credentials'] = "true" # allow send cookies
             return resp
         return wrapper_func
     return decorator
